ee433c793e5a56547223809dd234bae6d73492ea
Ramkumar Velmurugan <vrk1990@outlook.com>
2021-12-21 21:02:50 -0500

    Navigation wired up

---
+++ Pokedex/Pages/DetailView.swift
@@ -0,0 +1,14 @@
+//
+//  DetailView.swift
+//  Pokedex
+//
+//  Created by Ram on 2021-12-21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View{
+        Text("Hello")
+    }
+}
